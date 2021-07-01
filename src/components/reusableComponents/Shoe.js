ebdea5130526330a9635c61eb42c1c9f56d226e0
Mayen Akpan <neroxma@yahoo.com>
2021-06-30 17:09:14 -0700

    Updates

---
+++ src/components/reusableComponents/Shoe.js
@@ -0,0 +1,6 @@
+import ShoeOne from "../img/shoe1.png";
+import ShoetTwo from "../img/shoe2.png";
+import ShoeThree from "../img/shoe3.png";
+import ShoeFour from "../img/shoe4.png";
+import ShoeFive from "../img/shoe5.png";
+import ShoeSix from "../img/shoe6.png";
