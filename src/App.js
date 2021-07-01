aba42c94f19cc32b63cffaf3a574929bf10a6a1e
Mayen Akpan <neroxma@yahoo.com>
2021-06-30 17:02:05 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome</h1>
+    </div>
+  );
+}
+
+export default App;
