d1cefa83c538d2eff7120e2aabde4c66814f1e54
Mayen Akpan <neroxma@yahoo.com>
2021-07-01 15:42:46 -0700

    Updated

--- src/components/reusableComponents/Shoe.js
+++ src/components/reusableComponents/Shoe.js
@@ -1,9 +1,9 @@
-import ShoeOne from "../img/shoe1.png";
-import ShoetTwo from "../img/shoe2.png";
-import ShoeThree from "../img/shoe3.png";
-import ShoeFour from "../img/shoe4.png";
-import ShoeFive from "../img/shoe5.png";
-import ShoeSix from "../img/shoe6.png";
+import ShoeOne from "../../img/shoe1.png";
+import ShoetTwo from "../../img/shoe2.png";
+import ShoeThree from "../../img/shoe3.png";
+import ShoeFour from "../../img/shoe4.png";
+import ShoeFive from "../../img/shoe5.png";
+import ShoeSix from "../../img/shoe6.png";
 
 export default [
   {
